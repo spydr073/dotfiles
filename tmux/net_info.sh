b19ff36407c8437aa13048e8878678ed66864133
spydr073 <spydr073@gmail.com>
2017-10-25 00:59:32 -0600

    added icons and files to ignore file

---
+++ tmux/net_info.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+WLAN=$(ifconfig wlp3s0 | grep 'inet ' | awk '{print $2}')
+ETH0=$(ifconfig enp2s0 | grep 'inet ' | awk '{print $2}')
+PUB_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+MAC=$(cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address)
+
+LOC_IP=""
+if [[ "$WLAN" != "" && "$ETH0" != "" ]]; then
+	LOC_IP="W:$WLAN E:$ETH0"
+elif [[ "$WLAN" != "" ]]; then
+	LOC_IP="$WLAN"
+elif [[ "$ETH0" != "" ]]; then
+	LOC_IP="$ETH0"
+fi
+
+echo "#[fg=#777777,bg=#222222] ${MAC} #[default]  #[fg=#777777,bg=#222222] ${LOC_IP} #[default]  #[fg=#777777,bg=#222222] ${PUB_IP} #[default]"
+
